54bf711c23d014429ac64bfb9fcaa2b4d55a4c96
linocatucci <Lino@Fernie.local>
2017-09-04 14:12:06 +0200

    start with refactoring YelpCamp to have SPA parts (create campground and create comments)

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,18 +1,30 @@
 // AJAX post route of new campground
 
 
-
-
-
-
-
 // new comment post route
 $('#new_comment').submit(function(e) {
     e.preventDefault();
     var formData = $(this).serialize();
     var formAction = $(this).attr('action')
     $.post(formAction, formData, function(data) {
-        debugger;
-        console.log(data);
+        $('#show_comment_block').append(`
+        <div class='row' id='show_comment_block'>
+        <div class='col-md-12'>
+            <hr>
+            <strong> <%= comment.author.username %></strong>
+            <span class='pull-right'>${data.created_at}.fromNow()%></span>
+            <p>
+                ${data.text}
+            </p>
+
+            <% if (currentUser && currentUser.username === comment.author.username ){ %>
+                <a class="btn btn-xs btn-warning" href="/campgrounds/<%=campground._id %>/comments/<%=comment._id %>/edit">Edit</a>
+                <form id="delete_form" action="/campgrounds/<%=campground._id %>/comments/${data._id}?_method=DELETE" method="POST">
+                    <input type="submit" class="btn btn-xs btn-danger" value="Delete">
+                </form>
+                <% } %>
+        </div>
+    </div>
+        `)
     });
 });
