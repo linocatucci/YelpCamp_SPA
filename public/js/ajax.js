5b2d75b5ceb35138efcbc86ce7d5ca0ea173d618
linocatucci <Lino@Fernie.local>
2017-09-04 14:50:46 +0200

    start with refactoring YelpCamp to have SPA parts (create campground and create comments)

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -5,20 +5,20 @@
 $('#new_comment').submit(function(e) {
     e.preventDefault();
     var formData = $(this).serialize();
-    var formAction = $(this).attr('action')
+    var formAction = $(this).attr('action');
     $.post(formAction, formData, function(data) {
         $('#show_comment_block').append(`
         <div class='row' id='show_comment_block'>
         <div class='col-md-12'>
             <hr>
             <strong> <%= comment.author.username %></strong>
-            <span class='pull-right'>${data.created_at}.fromNow()%></span>
+            <span class='pull-right'><%=moment(${data.created_at}).fromNow()%></span>
             <p>
                 ${data.text}
             </p>
 
             <% if (currentUser && currentUser.username === comment.author.username ){ %>
-                <a class="btn btn-xs btn-warning" href="/campgrounds/<%=campground._id %>/comments/<%=comment._id %>/edit">Edit</a>
+                <a class="btn btn-xs btn-warning" href="/campgrounds/<%=campground._id %>/comments/${data._id}/edit">Edit</a>
                 <form id="delete_form" action="/campgrounds/<%=campground._id %>/comments/${data._id}?_method=DELETE" method="POST">
                     <input type="submit" class="btn btn-xs btn-danger" value="Delete">
                 </form>
